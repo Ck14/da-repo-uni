dc2529bd31743d8f70e5ab72f650fda6887d1f78
Ckevyn Omar Ovalle Sequén <covalle@minfin.gob.gt>
2025-07-18 16:18:08 -0600

    (+) config data source api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DataService } from './data/data.service';
 import { DataController } from './data/data.controller';
 import { HttpModule } from '@nestjs/axios';
+import { DataModule } from './data/data.module';
 
 @Module({
-  imports: [UsersModule, HttpModule],
-  controllers: [AppController, DataController],
-  providers: [AppService, DataService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    HttpModule,
+    DataModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule { }
--- src/data/data.service.ts
+++ src/data/data.service.ts
@@ -3,20 +3,33 @@ import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 import { writeFileSync, readFileSync, existsSync } from 'fs';
 import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class DataService implements OnModuleInit {
     private data: any = null;
     private readonly dataFile = join(__dirname, '../../data.json');
-    private readonly apiUrl = 'https://datos.minfin.gob.gt/api/action/datastore_search';
-    private readonly apiBody = {
-        resource_id: '85587118-152d-4613-bee0-d01d85ebdb9d',
-        filters: {
-            codigoEntidad: 12100401,
-        },
-    };
+    private readonly apiUrl: string;
+    private readonly resourceId: string;
+    private readonly codigoEntidad: string;
 
-    constructor(private readonly httpService: HttpService) { }
+    constructor(
+        private readonly httpService: HttpService,
+        private readonly configService: ConfigService,
+    ) {
+        this.apiUrl = this.configService.get<string>('API_URL') || '';
+        this.resourceId = this.configService.get<string>('RESOURCE_ID') || '';
+        this.codigoEntidad = this.configService.get<string>('CODIGO_ENTIDAD') || '';
+    }
+
+    private get apiBody() {
+        return {
+            resource_id: this.resourceId,
+            filters: {
+                codigoEntidad: this.codigoEntidad,
+            },
+        };
+    }
 
     async onModuleInit() {
         // Al iniciar el módulo, intenta cargar el archivo si existe
