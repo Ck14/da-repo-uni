b8b9a4b8a7edfc7bd0b6f4457cc3c1e958a7a54b
Ckevyn Ovalle <ckomarovalle@gmail.com>
2025-07-28 17:40:16 -0600

    (+) contacto

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://167.88.46.48:3000'], // o '*' para todos los orígenes (no recomendado en producción)
+    origin: ['http://localhost:3000'], // o '*' para todos los orígenes (no recomendado en producción)
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+  app.setGlobalPrefix('api'); // Agrega el prefijo /api/ a todas las rutas
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 3000;
+  const port = configService.get<number>('PORT') || 3100;
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
